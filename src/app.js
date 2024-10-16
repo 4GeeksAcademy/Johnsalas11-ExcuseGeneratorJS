bc752a0231c4444c1374fc2c84ca87be28634766
John <salasduranjohn96@gmail.com>
2024-10-16 18:11:46 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  alert("Para generar tu excusa ¡pulsa en el botón rosa!");
+
+  function generarExcusa() {
+    let who = ["Link", "Mario", "Zelda", "Luigi", "Sonic", "Pikachu"];
+
+    let what = [
+      "se tragó",
+      "me robó",
+      "superaumentó",
+      "rompió",
+      "teletransportó",
+      "encogió"
+    ];
+    let object = [
+      "mis setas especiales",
+      "mi mapa del laberinto",
+      "mi varita mágica",
+      "mis pokeballs",
+      "mi motivación"
+    ];
+
+    let randomWho = Math.floor(Math.random() * who.length);
+    let randomWhat = Math.floor(Math.random() * what.length);
+    let randomobject = Math.floor(Math.random() * object.length);
+
+    document.querySelector(
+      "#excuse"
+    ).innerHTML = `${who[randomWho]} ${what[randomWhat]} ${object[randomobject]}!`;
+  }
+  document
+    .getElementById("button")
+    .addEventListener("click", () => generarExcusa());
 };
